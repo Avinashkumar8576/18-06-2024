b5a2d335d12e1f11314589fdfd3eb38671ea4d35
Avinash Kumar <avinashkumar90089008@gmail.com>
2024-06-19 21:45:05 +0530

    Add files via upload

---
+++ JavaBasics/Example1.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		int a = 2, b = 3;
+		
+		System.out.println((a*a)+(2*a*b)+(b*b));
+						//2*2 + 2*2*3+3*3
+						//4+12+9
+						//25
+		
+	}
+	
+}
+
---
+++ JavaBasics/Hello.java
@@ -0,0 +1,13 @@
+package JavaBasics;
+
+public class Hello {
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World..!");
+		System.out.println(56);
+	}
+
+}
+
